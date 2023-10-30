af05b098b1a472d617b8ac3bc8fd65fbb7bb28ce
RTG-Dev <omer.golan.rtg@gmail.com>
2023-10-30 17:30:23 +0200

    testing

--- routes/pdfjs.js
+++ routes/pdfjs.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // get file to send 
 router.get('/',(req,res)=>{
     console.log('response  on get NDA 1 :')
-   res.sendFile(path.join(__dirname,'../pdfjs/build/pdf.js')  , options)
+   res.sendFile(path.join(__dirname,'./pdfjs/build/pdf.js')  , options)
   });
 
 module.exports = router;
