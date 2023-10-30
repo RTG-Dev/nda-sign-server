edca7a9c2cb820f876e0259a1d6daeb5703361ca
RTG-Dev <omer.golan.rtg@gmail.com>
2023-10-30 17:32:40 +0200

    testing

--- routes/pdfjs.js
+++ routes/pdfjs.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+var options = {
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true,
+        'name': 'OmerGolan',
+        'origin':'my-server' 
+    }
+  };
+
 // get file to send 
 router.get('/',(req,res)=>{
     console.log('response  on get NDA 1 :')
