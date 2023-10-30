f1b7c02c34dc569c4fb5dffe04fc9e30f58a76a4
RTG-Dev <omer.golan.rtg@gmail.com>
2023-10-30 18:00:39 +0200

    testing

--- routes/pdfjs.js
+++ routes/pdfjs.js
@@ -12,11 +12,11 @@ var options = {
   };
 
 // get file to send 
-router.get('/',(req,res)=>{
+router.get('/pdf.js',(req,res)=>{
     console.log('response on get to - pdfjs.js  :',__dirname)
     res.sendFile(path.join(__dirname,'../pdfjs/build/pdf.js')  , options)
 });
-router.get('/pdf.worker',(req,res)=>{
+router.get('/pdf.worker.js',(req,res)=>{
     console.log('response on get to - pdf.worker  :',__dirname)
     res.sendFile(path.join(__dirname,'../pdfjs/build/pdf.worker.js')  , options)
 });
