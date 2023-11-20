f0dc655e6800f0d6ecfcf064c7c7ab0f2e844394
RTG-Dev <omer.golan.rtg@gmail.com>
2023-11-20 18:45:12 +0200

    on pdfSigned route

--- routes/pdfSigned.js
+++ routes/pdfSigned.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-
 const pdfSignedController = require('../controllers/pdfSignedController');
-
-
-// incoming pdf from vanila javascript project
-router.post('/',(req, res) => pdfSignedController.savePdf(req, res)); 
-// incoming pdf from angular project
-router.post('/angularProject', upload.single('file'),(req, res) => pdfSignedController.savePdfAngularProject(req, res));
 
 // middleware to get pdf file from front
 const storage = multer.diskStorage({
@@ -22,5 +15,11 @@ const storage = multer.diskStorage({
   })
   
 const upload = multer({ storage: storage });
+
+// incoming pdf from vanila javascript project
+router.post('/',(req, res) => pdfSignedController.savePdf(req, res)); 
+// incoming pdf from angular project
+router.post('/angularProject', upload.single('file'),(req, res) => pdfSignedController.savePdfAngularProject(req, res));
+
 
 module.exports = router;
