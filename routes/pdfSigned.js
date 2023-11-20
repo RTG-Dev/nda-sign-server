b7641aa1b8f8c2aa232494f64fce69665346d2d3
RTG-Dev <omer.golan.rtg@gmail.com>
2023-11-20 18:54:27 +0200

    on pdfSigned route

--- routes/pdfSigned.js
+++ routes/pdfSigned.js
@@ -7,7 +7,7 @@ const pdfSignedController = require('../controllers/pdfSignedController');
 // middleware to get pdf file from front
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, './docs/savedPDF') // Ensure this directory exists
+      cb(null, './docs/nda') // Ensure this directory exists
     },
     filename: function(req, file, cb) {
         cb(null,  new Date().toLocaleDateString("he-IL").split("/") +'-'+  file.originalname)  // path.extname(file.originalname))
